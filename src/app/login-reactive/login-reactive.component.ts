fcbb263d652c3721fb74d9b9990a9f4bf769bc31
Margo Yermachenko <margoermachenko@gmail.com>
2024-05-09 19:07:38 +0200

    Login form on reactive style with FormGroup

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { createPasswordStrengthValidator } from '../validators/password-strength.validator';
 
 
 @Component({
@@ -7,7 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-reactive.component.css']
 })
 export class LoginReactiveComponent implements OnInit {
+  //different way to define form control; in template - [formControl]
+  password = new FormControl('', {
+    validators: [
+      Validators.required, 
+      Validators.minLength(8),
+      createPasswordStrengthValidator(), //PasswordStrengthDirective(TD form)
+    ],
+     
+  });
 
+
+  //here we defining form (1)
+  form = new FormGroup ({
+    //here we defining form controls with initial value '' (2)
+    email: new FormControl('', {
+      //here we defining validators to this form control
+      validators: [Validators.required, Validators.email,],      
+      updateOn: 'blur', //for update value after loosing focus
+    }),  
+    password: this.password,
+  }); 
 
   constructor() {
 
