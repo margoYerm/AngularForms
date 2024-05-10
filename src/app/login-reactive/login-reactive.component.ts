f138cd73ddb0271272fb680c6933cf8315eebda2
Margo Yermachenko <margoermachenko@gmail.com>
2024-05-10 13:49:24 +0200

    Types in form. NonNullable forms

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -11,17 +11,27 @@ import { createPasswordStrengthValidator } from '../validators/password-strength
 export class LoginReactiveComponent implements OnInit { 
   form = this.fb.group({
     //email - is name of control, '' - initial value, [] - validators
-    email: ['', {
+    email: ['', {//union type: '' | null by default
       validators: [Validators.required, Validators.email],
       updateOn: 'blur',
-    }], 
+    }],
+    //if we don't to email value can be null we can use this way of declaration
+    // and after calling reset() this value will be '' (initial value)
+    /*email: this.fb.nonNullable.control('', {
+      validators: [Validators.required, Validators.email],
+      updateOn: 'blur',
+    }), */
     password: ['', [
       Validators.required, 
       Validators.minLength(8),
       createPasswordStrengthValidator(), //PasswordStrengthDirective(TD form)      
     ]],
+
+
   })
 
+  //When we want to make nonNullable all form, we can use NonNullableFormBuilder
+  // and declaring form like with FormBuilder 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {}
@@ -33,5 +43,15 @@ export class LoginReactiveComponent implements OnInit {
 
   get password() {
     return this.form.controls['password']
+  }
+
+  login() {
+    const formValue = this.form.value;
+    this.form.patchValue({})
+  }
+
+  reset() {
+    this.form.reset(); //build in method for forms, witch set all values = null 
+    console.log(this.form.value);
   }
 }
