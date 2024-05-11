07335b0306ad0c1207b73d177fe3755ae893df56
Margo Yermachenko <margoermachenko@gmail.com>
2024-05-11 16:09:49 +0200

    Implemented Async validator function (FormBuilder, getter, error) for check data at the server. Angular Material Stepper (for input)

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CoursesService} from '../../services/courses.service';
 import {Observable} from 'rxjs';
 import {filter} from 'rxjs/operators';
+import { courseTitleValidator } from '../../validators/course-title.validator';
 
 @Component({
   selector: 'create-course-step-1',
@@ -10,9 +11,26 @@ import {filter} from 'rxjs/operators';
   styleUrls: ['./create-course-step-1.component.scss']
 })
 export class CreateCourseStep1Component implements OnInit {
+  form = this.fb.group({
+    title: ['', {
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60)
+      ],
+      asyncValidators: [courseTitleValidator(this.courses)],
+      updateOn: 'blur', //for check input after focus loose
+    }]
+  })
 
-  ngOnInit() {
+  // inject FormBuilder service
+  constructor(
+    private fb: FormBuilder,
+    private courses: CoursesService
+    ) {}
+  ngOnInit() {}
 
+  get courseTitle() {
+    return this.form.controls['title'];
   }
-
 }
